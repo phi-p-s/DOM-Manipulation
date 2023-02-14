18bd548fd2a8290ae61643965cf549da337ca10b
phil-p-s <pstanton@mines.edu>
2023-02-13 21:50:54 -0700

    Testing idk

--- pokegetter.js
+++ pokegetter.js
@@ -1,1 +1,14 @@
-const pokeName = document.getElementById("pname");
+const api = "https://pokeapi.co/api/v2/"
+var pokeName = document.getElementById("pname");
+var fetchName = api.concat(pokeName)
+let addButton = document.querySelector('#button');
+addButton.addEventListener("click", ()=>{
+    msg.classList.toggle('reveal');
+});
+
+function getPokemon(apiLink){
+    console.log("HI");
+    fetch(apiLink)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
